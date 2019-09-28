e55057b814cb44ef33325bbfeab62ce305fcb08a
MT2321 <mrodriguez@itba.edu.ar>
2019-09-28 04:27:44 -0300

    Updated Path

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('C:/Users/Msrt/Documents/GitHub/cid-docs/SimiLab'))
+sys.path.insert(0, os.path.abspath('../SimiLab'))
 
 
 # -- Project information -----------------------------------------------------
